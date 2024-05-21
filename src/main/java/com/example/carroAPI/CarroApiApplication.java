7272dc74269e6e74fda3458aca6930cc148f1689
Isadora Gil de Souza <105384553+isadoragil@users.noreply.github.com>
2024-05-21 17:20:15 -0300

    1

--- src/main/java/com/example/carroAPI/CarroApiApplication.java
+++ src/main/java/com/example/carroAPI/CarroApiApplication.java
@@ -1,13 +1,18 @@
 package com.example.carroAPI;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CarroApiApplication {
+public class CarroApiApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CarroApiApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
 }
---
+++ src/main/java/com/example/carroAPI/service/ConsumoAPI.java
@@ -0,0 +1,26 @@
+package com.example.carroAPI.service;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+import static java.net.http.HttpClient.newHttpClient;
+
+public class ConsumoAPI {
+    public String obterDados(String endereco) {
+        HttpClient client = newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(endereco))
+                .build();
+        HttpResponse<String> response = null;
+        try {
+            response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        return response.body();
+    }
+}
