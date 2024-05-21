b2a9d880b816fd5e1fcef5792ee2e82b0638cccb
Isadora Gil de Souza <105384553+isadoragil@users.noreply.github.com>
2024-05-21 13:42:43 -0300

    Initial commit

---
+++ src/main/java/com/example/carroAPI/CarroApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.carroAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarroApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarroApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/carroAPI/CarroApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.carroAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarroApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
